8f8fb3e835f56f483bc4c19d3c9527949df5d7be
Rakesh Paswan <rakesh25paswan@gmail.com>
2022-12-23 17:35:58 +0530

    added password encoder without Bencryptor

--- spring-boot-security-learn/src/main/java/com/learn/config/MySecurityConfig.java
+++ spring-boot-security-learn/src/main/java/com/learn/config/MySecurityConfig.java
@@ -1,11 +1,15 @@
 package com.learn.config;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -18,5 +22,15 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated()
                 .and()
                 .httpBasic();
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth)throws Exception{
+        auth.inMemoryAuthentication().withUser("rakesh").password("123rak").roles("NORMAL");
+        auth.inMemoryAuthentication().withUser("vikash").password("123vik").roles("ADMIN");
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
     }
 }
